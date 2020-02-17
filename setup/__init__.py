5dbc93c166208ce80cf8551fc7e03a0a13dec317
Shapkofil <ldshapkofil@gmail.com>
2020-02-18 00:18:13 +0200

    Linux zip install fixed

--- setup/__init__.py
+++ setup/__init__.py
@@ -8,7 +8,13 @@ from subprocess import call
 def unpack():
     if platform.system() == 'Linux':
         try:
-            call(['sh', join(getcwd(), 'linux_setup.sh')])
+            with open(join(getcwd(), 'linux_setup.sh'), 'r') as f:
+                old = f.read()
+
+            file = join(getcwd(), 'linux_exec.sh')
+            with open(file, 'w') as f:
+                f.write('cd {}\n'.format(getcwd()) + old)
+            call(['sh', file])
         except RuntimeError:
             print('Virtualenv not installed')
 
---
+++ setup/linux_exec.sh
@@ -0,0 +1,5 @@
+cd /home/shapkofil/.blender/dev/addons/liquidknot/setup
+cd ../openGL
+virtualenv .venv
+.venv/bin/pip install -r ../setup/requirements.txt
+mkdir temp
