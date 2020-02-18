74bad490784116edf8c3c1be3011eed356e6de5f
Shapkofil <ldshapkofil@gmail.com>
2020-02-18 12:11:20 +0200

    Windows Easy Way

--- setup/__init__.py
+++ setup/__init__.py
@@ -18,7 +18,19 @@ def unpack():
                 f.write('cd {}\n'.format(dirname(ap(__file__))) + old)
             call(['sh', file])
         except RuntimeError:
-            print('Virtualenv not installed')
+            print('Virtualenv might not installed')
+
+    if platform.system() == 'Windows':
+        try:
+            with open(join(dirname(ap(__file__)), 'win_setup.bat'), 'r') as f:
+                old = f.read()
+
+            file = join(dirname(ap(__file__)), 'win_exec.bat')
+            with open(file, 'w') as f:
+                f.write('cd {}\n'.format(dirname(ap(__file__))) + old)
+            call(['bat.exe', file])
+        except RuntimeError:
+            print('Virtualenv might not installed')
 
 
 if __name__ == "__main__":
---
+++ setup/win_setup.bat
@@ -0,0 +1,4 @@
+cd ../openGL
+virtualenv .venv
+.venv/bin/pip.exe install -r ../setup/requirements.txt
+mkdir temp
