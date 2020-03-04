9de624bed998395cad6b0fef75b2dc43fa51a631
Shapkofil <ldshapkofil@gmail.com>
2020-03-04 20:44:58 +0200

    Space in virtualenv Hotfix

--- setup/win_setup.bat
+++ setup/win_setup.bat
@@ -1,5 +1,5 @@
 cd ..\openGL
 pip install virtualenv --user
-python -m virtualenv %cd%\.venv
-%cd%\.venv\Scripts\pip.exe install -r %cd%\..\setup\requirements.txt
+python -m virtualenv "%cd%\.venv"
+"%cd%\.venv\Scripts\pip.exe" install -r "%cd%\..\setup\requirements.txt"
 mkdir temp
